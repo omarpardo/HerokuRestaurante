ba94e7b066dc10d6646353633c9d3ce02b05eba4
omar pardo <omarpardov@gmail.com>
2023-07-16 11:00:25 -0500

    Creación del proyecto

---
+++ src/main/java/com/curso/ecommerce/service/IOrdenService.java
@@ -0,0 +1,16 @@
+package com.curso.ecommerce.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.curso.ecommerce.model.Orden;
+import com.curso.ecommerce.model.Usuario;
+
+public interface IOrdenService {
+	List<Orden> findAll();
+	Optional<Orden> findById(Integer id);
+	Orden save (Orden orden);
+	String generarNumeroOrden();
+	String generarNumeroMesa(); 
+	List<Orden> findByUsuario (Usuario usuario);
+}
